1a95dbf60b4e3027b4eedcfcf549b71f2e073ab9
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-02-22 14:56:27 -0300

    layout clique voltar ao topo

--- js/script.js
+++ js/script.js
@@ -27,3 +27,9 @@ function animaCor() {
 animaCor();
 
 window.addEventListener('scroll', animaCor);
+ 
+
+var btn = document.querySelector(".back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
